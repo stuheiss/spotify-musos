e6d065538211415fe03f0b7574ba668c68aff361
Stu Heiss <sheiss@highlandsolutions.com>
2018-07-01 10:10:56 -0500

    Rework dataflow
    simplify, remove dataflow sorting
    add artist image
    add link to play artist
    add button to add related artist to query
    fix display genres

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ const spotifyApi = new SpotifyWebApi()
 
 const Task = require('data.task')
 const { List } = require('immutable-ext')
-const { Pair, Sum } = require('./monoid')
+//const { Pair, Sum } = require('./monoid')
 const Either = require('data.either')
 
 class App extends Component {
@@ -22,7 +22,6 @@ class App extends Component {
       loggedIn: token ? true : false,
       nowPlaying: { name: 'Not Checked', albumArt: '' },
       value: '',
-      artist: '',
       related: []
     }
     this.handleChange = this.handleChange.bind(this)
@@ -40,7 +39,6 @@ class App extends Component {
 
   dataflow(query) {
     if (!query) return
-    const threshhold = 0
     const names = new Task((rej, res) =>
       res(query.split(',').map(s => s.trim()))
     )
@@ -66,52 +64,55 @@ class App extends Component {
     const relatedArtists = id =>
       relatedArtistsTask(id).map(result => result.artists)
 
+    // remove one level of array nesting
+    const flatten = xs => xs.reduce((acc, cur) => acc.concat(cur), [])
+
     const related = name =>
       findArtist(name)
         .map(artist => artist.id)
         .chain(relatedArtists)
-        .map(artists => artists.map(artist => artist.name))
 
-    // reduce an array with dups to an array of elems with counts
-    const rank = xs =>
-      xs.reduce((acc, cur) => {
-        acc[cur] = (acc[cur] || 0) + 1
-        return acc
-      }, {})
+    const dumper = x => {
+      console.log(x)
+      return x
+    }
 
-    // remove one level of array nesting
-    const flatten = arr =>
-      arr.reduce((acc, cur) => {
-        return (acc = acc.concat(cur))
-      }, [])
-
-    // transform array of elems with counts to array of objects for easy sorting
-    const toArr = data =>
-      Object.keys(data).map(key => ({ key, value: data[key] }))
-
-    // sort objects by value
-    const byValue = ({ value: v1 }, { value: v2 }) => v2 - v1
-
-    // return a sorted ranking of related artists that have counts at least 2
-    const sortedRank = xs =>
-      toArr(rank(flatten(xs)))
-        .sort(byValue)
-        .filter(({ value: v }) => v > threshhold)
+    const id = x => x
 
     const main = names =>
       List(names)
         .traverse(Task.of, related)
-        //.map(artistIntersection)
-        .map(sortedRank)
+        .map(artists =>
+          flatten(artists)
+            .map(id)
+            .map(artist => ({
+              name: artist.name,
+              id: artist.id,
+              popularity: artist.popularity,
+              genres: artist.genres,
+              image: artist.images[0] ? artist.images[0].url : '',
+              spotify: artist.external_urls.spotify
+            }))
+        )
 
-    const updateState = related => {
+    // xs is array of hash
+    const process = xs => {
+      const rels = xs.reduce((acc, cur) => {
+        let cnt = acc[cur.name] && acc[cur.name].count ? acc[cur.name].count : 0
+        acc[cur.name] = Object.assign(cur, { count: cnt + 1 })
+        return acc
+      }, {})
+      const relsAry = Object.values(rels).map(el =>
+        Object.assign(el, { score: el.count * el.popularity })
+      )
+
       this.setState({
-        related: related
+        related: relsAry
       })
     }
 
     // run it
-    names.chain(main).fork(console.error, updateState)
+    names.chain(main).fork(console.error, process)
   }
 
   getHashParams() {
@@ -138,22 +139,65 @@ class App extends Component {
     })
   }
 
+  handleAddToArtists(name) {
+    const value = this.state.value ? this.state.value + ',' + name : name
+    this.setState({ value: value })
+  }
+
   render() {
+    const loggedIn = this.state.loggedIn
     const data = this.state.related
-      ? this.state.related.map(item => ({
-          artist: item.key,
-          count: item.value
-        }))
-      : []
     const columns = [
       {
         Header: 'Artist',
-        accessor: 'artist' // String-based value accessors!
+        accessor: 'name', // String-based value accessors!
+        Cell: props => (
+          <div>
+            <button onClick={() => this.handleAddToArtists(props.value)}>
+              Add
+            </button>
+            <br />
+            <span className="text">{props.value}</span>
+          </div>
+        )
       },
       {
         Header: 'Count',
         accessor: 'count',
+        minWidth: 50,
         Cell: props => <span className="number">{props.value}</span> // Custom cell components!
+      },
+      {
+        Header: 'Popularity',
+        accessor: 'popularity',
+        minWidth: 50,
+        Cell: props => <span className="number">{props.value}</span> // Custom cell components!
+      },
+      {
+        Header: 'Score',
+        accessor: 'score',
+        minWidth: 50,
+        Cell: props => <span className="number">{props.value}</span> // Custom cell components!
+      },
+      {
+        Header: 'Genres',
+        accessor: 'genres',
+        minWidth: 200,
+        className: 'genres',
+        Cell: props => <span className="genres">{props.value.join(', ')}</span>
+      },
+      {
+        Header: 'Play',
+        accessor: 'image',
+        Cell: props => {
+          return (
+            <div>
+              <a href={props.value}>
+                {props.value && <img alt="" height={100} src={props.value} />}
+              </a>
+            </div>
+          )
+        }
       }
     ]
     return (
@@ -167,28 +211,33 @@ class App extends Component {
             style={{ height: 150 }}
           />
         </div>
-        {this.state.loggedIn && (
+        {loggedIn && (
           <button onClick={() => this.getNowPlaying()}>
             Check Now Playing
           </button>
         )}
-        <div>
-          Related Artists
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Artists:
-              <input
-                type="text"
-                value={this.state.value}
-                onChange={this.handleChange}
-              />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-        <div>
-          <ReactTable data={data} columns={columns} />
-        </div>
+        {loggedIn && (
+          <div>
+            Related Artists
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Artists:
+                <input
+                  style={{ width: '370px', fontSize: '20px' }}
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        )}
+        {loggedIn && (
+          <div>
+            <ReactTable pageSize={100} data={data} columns={columns} />
+          </div>
+        )}
       </div>
     )
   }
